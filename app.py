2cbac2621c90e47e574eac60d24c5610d7939d01
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-11-26 01:42:22 +0300

    empty commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
@@ -16,14 +16,19 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
+
+        #check email and password from firestore
         try:
-            user = auth.get_user_by_email(username)  # Assuming you use email as the username
-            auth.verify_password(password, user.password_hash)
-            return "Login successful"  # You can redirect to another page if login is successful
-        except Exception as e:
-            return f"Login failed: {e}"
+            user = auth.get_user_by_email(email)
+            if user:
+                auth.sign_in_with_email_and_password(email, password)
+                return redirect(url_for('dashboard'))
+        except:
+            return "Invalid email or password"
+
+        
 
     return render_template('login.html')
 
@@ -59,6 +64,10 @@ def register():
             return f"Registration failed: {e}"
 
     return render_template('register.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5050, debug=True)
