f66148ef16ebcb9424229e0ea73e82b55cc9f2b1
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-11-25 17:15:49 +0300

    user-auth

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth, firestore
 
 cred = credentials.Certificate("KEYS\\user-api.json")
 firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
+db = firestore.client()
 
 @app.route('/')
 def index():
@@ -26,8 +27,37 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            return "Password and Confirm Password do not match"
+        try:
+            # Create user in Firebase Authentication
+            user = auth.create_user(
+                email=email,
+                email_verified=False,
+                password=password,
+                display_name=username
+            )
+            # Save additional user information to Firestore database
+            user_data = {
+                'uid': user.uid,
+                'username': username,
+                'email': email
+                # Add more user data fields as needed
+            }
+            db.collection('users').add(user_data)
+
+            return "Registration successful"  # You can redirect to another page if registration is successful
+        except Exception as e:
+            return f"Registration failed: {e}"
+
     return render_template('register.html')
 
 if __name__ == '__main__':
