87c9536a63d27410c13ba5c0bcfc7dcab228b49c
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-11-26 14:49:45 +0300

    Login, Register Completly Implemented

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
+from pyrebase import pyrebase
 
+# Initialize Firebase Admin SDK
 cred = credentials.Certificate("KEYS\\user-api.json")
 firebase_admin.initialize_app(cred)
+
+firebase_config = {
+  "apiKey": "AIzaSyAhs8VGY6wvs_kG5ujPdNkZAC7DAoTmPN4",
+  "authDomain": "web-test-aa01a.firebaseapp.com",
+  "databaseURL": "https://web-test-aa01a-default-rtdb.firebaseio.com",
+  "projectId": "web-test-aa01a",
+  "storageBucket": "web-test-aa01a.appspot.com",
+  "messagingSenderId": "319219619955",
+  "appId": "1:319219619955:web:0a44e3866f9c3c5ee93206",
+  "measurementId": "G-WCJEGP2Z1G"
+}
+
+
+firebase = pyrebase.initialize_app(firebase_config)
+
+auth1 = firebase.auth()
 
 app = Flask(__name__)
 db = firestore.client()
@@ -18,18 +36,16 @@ def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-
+        
+        user = auth.get_user_by_email(email)
         #check email and password from firestore
-        try:
+        try:            
             user = auth.get_user_by_email(email)
             if user:
-                auth.sign_in_with_email_and_password(email, password)
+                auth1.sign_in_with_email_and_password(email, password)
                 return redirect(url_for('dashboard'))
         except:
             return "Invalid email or password"
-
-        
-
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -54,7 +70,8 @@ def register():
             user_data = {
                 'uid': user.uid,
                 'username': username,
-                'email': email
+                'email': email,
+                'password': password
                 # Add more user data fields as needed
             }
             db.collection('users').add(user_data)
